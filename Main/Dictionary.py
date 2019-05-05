ce218a0fbd487859b49bc618535acb04428a7923
Julian Chong <julianchong10@gmail.com>
2019-05-05 21:59:11 +0800

    Updated Dictionary class

--- Main/Dictionary.py
+++ Main/Dictionary.py
@@ -7,15 +7,39 @@ class Dictionary:
     is n-gram : WordInformation, where WordInformation is a class.
 
     Attributes:
-        dictionary (dictionary): The dictionary to store n-grams
+        MAX_RATING (int): The maximum rating
+        dictionary (dict): The dictionary to store n-grams
+        totalTerms (int[]): To keep track of number of terms that appear
+                            in each class rating
     """
 
-    def __init__(self):
+    def __init__(self, maxRating):
         """
         Constructor method.
         """
+        self.MAX_RATING = maxRating
         self.dictionary = {}
+        self.totalTerms = [0] * (self.MAX_RATING + 1)
 
+
+    def extractWords(self, inputDataset):
+        reviewFile = open(inputDataset, "r", encoding="utf-8-sig")
+        for record in reviewFile:
+            record = record.strip().split("\t")     # tab-delimited .txt file 
+            self.addUnigrams(int(record[0]), record[1])
+        reviewFile.close()
+
+
+    def getTotalTerms(self):
+        """
+        Returns:
+            A copy array of the attribute totalTerms.
+        """
+        copyArray = []
+        for value in self.totalTerms:
+            copyArray.append(value)
+        return copyArray
+    
 
     def addItem(self, key):
         """
@@ -31,19 +55,7 @@ class Dictionary:
         """
         if key in self.dictionary:
             raise Exception("Key already exist in dictionary")
-        self.dictionary[key] = WordInformation()
-        
-        
-    def scanSentence(self, rating, writtenReview):
-        """
-        Add unigrams, then bigrams from a written review.
-
-        Arguments:
-            rating (int): The rating given for a written review
-            writtenReview (str): The written review
-        """
-        self.addUnigrams(rating, writtenReview)
-        self.addBigrams(rating, writtenReview)
+        self.dictionary[key] = WordInformation(self.MAX_RATING)
 
 
     def addUnigrams(self, rating, writtenReview):
@@ -60,6 +72,7 @@ class Dictionary:
                 if word not in self.dictionary:
                     self.addItem(word)
                 self.dictionary[word].incrementFrequency(rating)
+                self.totalTerms[rating] += 1
 
 
     def addBigrams(self, rating, writtenReview):
@@ -83,7 +96,15 @@ class Dictionary:
                 self.dictionary[bigram].incrementFrequency(rating)
 
 
-    def computeTFIDF(self, MAX_RATING):
+    def computeTF(self):
+        """
+        Compute the TF for each word in the dictionary
+        """
+        for word in self.dictionary:
+            self.dictionary[word].setTF(self.getTotalTerms())
+
+
+    def computeTFIDF(self):
         """
         Compute the TFIDF scores for each n-gram in the dictionary
 
@@ -92,12 +113,11 @@ class Dictionary:
         """
         for word in self.dictionary:
             numOfAppearance = self.dictionary[word].getNumRatingsWordAppears()
-            idf = math.log( (MAX_RATING + 1) / (numOfAppearance + 1) )
+            idf = math.log( (self.MAX_RATING) / (numOfAppearance), 10 )
             self.dictionary[word].setTFIDF(idf)
 
 
     def predictRating(self, writtenReview):
-        totalKeywords = [0] * 6
         totalScores = [0] * 6
         sentence = writtenReview.split()
         
@@ -106,14 +126,6 @@ class Dictionary:
                 wordScores = self.dictionary[word].getTFIDF()
                 for i in range(1, len(totalScores)):
                     if wordScores[i] != 0:
-                        totalKeywords[i] += 1
                         totalScores[i] += wordScores[i]
 
-        indexOfHighestScore = 1
-        for i in range(1, len(totalKeywords)):
-            if totalScores[i] != 0 and totalKeywords[i] != 0:
-                totalScores[i] /= totalKeywords[i]
-                if totalScores[i] > totalScores[indexOfHighestScore]:
-                    indexOfHighestScore = i
-      
-        return indexOfHighestScore
+        return totalScores.index(max(totalScores))
