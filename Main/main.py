d2c76013e437610cf4fd434452410de0ef5c5cc8
Julian Chong <julianchong10@gmail.com>
2019-05-05 22:01:38 +0800

    Updated main.py

--- Main/main.py
+++ Main/main.py
@@ -1,24 +1,22 @@
-from WordInformation import WordInformation
 from Dictionary import Dictionary
 
-def extractWords(filename):
-    dictionary = Dictionary()
-    reviewFile = open(filename, "r", encoding="utf-8-sig")
-    for record in reviewFile:
-        record = record.strip().split("\t")     # tab-delimited .txt file 
-        dictionary.scanSentence(int(record[0]), record[1])
-    reviewFile.close()
-    return dictionary
+def getDictionary(inputDataset):
+    wordDictionary = Dictionary(MAX_RATING)
+    wordDictionary.extractWords(inputDataset)
+    wordDictionary.computeTF()
+    wordDictionary.computeTFIDF()
+    
+    return wordDictionary    
 
     
-def runTest(dictionary, filename):
+def runTest(dictionary, datasetToTestOn):
     totalRecords = 0
     correctlyPredicted = 0
-    reviewFile = open(filename, "r", encoding="utf-8-sig")
+    reviewFile = open(datasetToTestOn, "r", encoding="utf-8-sig")
     
     for record in reviewFile:
-        record = record.strip().split("\t") # tab-delimited .txt file
         totalRecords += 1
+        record = record.strip().split("\t") # tab-delimited .txt file
         if dictionary.predictRating(record[1]) == int(record[0]):
             correctlyPredicted += 1  
       
@@ -31,23 +29,19 @@ def runTest(dictionary, filename):
 if __name__ == "__main__":
 
     MAX_RATING = 5
-
+    
     print("\nWith stop words and no lemmatization")
-    wordDictionary = extractWords("dataset-preprocessed-01.txt")
-    wordDictionary.computeTFIDF(MAX_RATING)
+    wordDictionary = getDictionary("dataset-preprocessed-01.txt")
     runTest(wordDictionary, "dataset-amazon.txt")
 
     print("\nRemoved stop words")
-    wordDictionary = extractWords("dataset-preprocessed-02.txt")
-    wordDictionary.computeTFIDF(MAX_RATING)
+    wordDictionary = getDictionary("dataset-preprocessed-02.txt")
     runTest(wordDictionary, "dataset-amazon.txt")
 
     print("\nWith lemmatization")
-    wordDictionary = extractWords("dataset-preprocessed-03.txt")
-    wordDictionary.computeTFIDF(MAX_RATING)
+    wordDictionary = getDictionary("dataset-preprocessed-03.txt")
     runTest(wordDictionary, "dataset-amazon.txt")
 
     print("\nRemove stop words and has been lemmatized")
-    wordDictionary = extractWords("dataset-preprocessed-04.txt")
-    wordDictionary.computeTFIDF(MAX_RATING)
+    wordDictionary = getDictionary("dataset-preprocessed-04.txt")
     runTest(wordDictionary, "dataset-amazon.txt")
