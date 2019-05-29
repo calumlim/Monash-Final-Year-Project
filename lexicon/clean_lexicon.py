bdbb614dd55f9c98ec0b3d361ff9d5312ad4165c
Calum Lim Sheng En <calumstah@gmail.com>
2019-05-29 23:37:00 +0800

    Removed unnecessary directories

--- lexicon/clean_lexicon.py
+++
@@ -1,7 +0,0 @@
-f = open("lexicon.txt", "r")
-w = open("lexicon_clean.txt", "w")
-
-for line in f:
-    line = line.split("\t")
-    final_str = line[0] + "\t" + line[1] + "\n"
-    w.write(final_str)
