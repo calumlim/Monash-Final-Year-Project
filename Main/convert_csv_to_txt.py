315eb51b5d9fa5976e5bd652ab0eb9a80af565b7
calumlim <calumstah@gmail.com>
2019-05-30 18:08:16 +0800

    Added documentation & remove useless rows feature for csv to text file

--- Main/convert_csv_to_txt.py
+++ Main/convert_csv_to_txt.py
@@ -1,24 +1,31 @@
 import csv
 
 def convert(filename, ratingCol, textReviewCol):
+    """
+    Convert a .csv file to .txt file
+
+    Arguments:
+        filename (str): name of .csv file
+        ratingCol (str): letter of the column for rating
+        textReviewCol (str): letter of the column for written review
+    """
     ratingCol = ord(ratingCol.lower()) - 97
     textReviewCol = ord(textReviewCol.lower()) - 97
     
     outputTxtFilename = filename.split(".")[0] + ".txt"
     outputString = ""
 
-    with open(filename, 'r', errors="ignore") as csvFile:
+    with open(filename, 'r', errors="ignore", encoding="utf-8-sig") as csvFile:
         reader = csv.reader(csvFile)
         for row in reader:
-            outputString += row[ratingCol] + "\t" + row[textReviewCol] + "\n"
+            if row[ratingCol] != "" and row[textReviewCol] != "":
+                outputString += row[ratingCol] + "\t" + row[textReviewCol] + "\n"
     csvFile.close()
 
-    txtFile = open(outputTxtFilename, 'w')
+    txtFile = open(outputTxtFilename, 'w', errors="ignore")
     txtFile.write(outputString.rstrip())
     txtFile.close()
     
     return
-
-
     
     
