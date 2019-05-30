c56dac124c96bfd9a49690c606bd96cb5a3b29da
Bourn3x <julianchong10@gmail.com>
2019-05-30 22:44:24 +0800

    Added command line instructions to install libraries

---
+++ Main/download_modules.py
@@ -0,0 +1,14 @@
+import nltk
+import os
+
+# COMMAND LINE INSTRUCTIONS TO INSTALL LIBRARIES
+os.system("pip install nltk")
+os.system("pip install sklearn")
+os.system("pip install pandas")
+os.system("pip install numpy")
+
+# TO INSTALL NLTK SUBPACKAGES
+nltk.download("stopwords")
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
--- Main/download_nltk_subpackages.py
+++
@@ -1,7 +0,0 @@
-# NLTK MUST BE FIRST INSTALLED BEFORE RUNNING THIS SCRIPT
-# type in command line pip install nltk
-import nltk
-nltk.download("stopwords")
-nltk.download('wordnet')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
